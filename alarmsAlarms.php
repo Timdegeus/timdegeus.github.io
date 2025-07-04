8fe9f9552494794ec31b0663db6ce59e861ba790
Tim de Geus <tim.de.geus@student.nhlstenden.com>
2025-07-04 16:58:49 +0200

    Changed widgets on dashboard

--- alarmsAlarms.php
+++ alarmsAlarms.php
@@ -8,6 +8,56 @@
 ?>
 <div id="blok2">
     <?php include("header-content.php"); ?>
+    <div class="alarmContent">
+        <div class="alarmWidget">
+            <h5>[12] Refrigerator</h5>
+            <p class="alarmWidgetP">Alarm: test alarm SMS en email</p>
+            <p class="alarmWidgetP mobile">Trigger: Temperature < 25 C</p>
+            <p class="alarmWidgetP mobile">Triggered on:</p>
+            <p class="alarmWidgetP mobile">2024-09-16 08:36:52</p>
+            <p class="alarmWidgetP mobile">current: 11.1 C</p>
+        </div>
+        <div class="alarmWidget">
+            <h5>[12] Refrigerator</h5>
+            <p class="alarmWidgetP">Alarm: test alarm SMS en email</p>
+            <p class="alarmWidgetP mobile">Trigger: Temperature < 25 C</p>
+            <p class="alarmWidgetP mobile">Triggered on:</p>
+            <p class="alarmWidgetP mobile">2024-09-16 08:36:52</p>
+            <p class="alarmWidgetP mobile">current: 11.1 C</p>
+        </div>
+        <div class="alarmWidget">
+            <h5>[12] Refrigerator</h5>
+            <p class="alarmWidgetP">Alarm: test alarm SMS en email</p>
+            <p class="alarmWidgetP mobile">Trigger: Temperature < 25 C</p>
+            <p class="alarmWidgetP mobile">Triggered on:</p>
+            <p class="alarmWidgetP mobile">2024-09-16 08:36:52</p>
+            <p class="alarmWidgetP mobile">current: 11.1 C</p>
+        </div>
+        <div class="alarmWidget">
+            <h5>[12] Refrigerator</h5>
+            <p class="alarmWidgetP">Alarm: test alarm SMS en email</p>
+            <p class="alarmWidgetP mobile">Trigger: Temperature < 25 C</p>
+            <p class="alarmWidgetP mobile">Triggered on:</p>
+            <p class="alarmWidgetP mobile">2024-09-16 08:36:52</p>
+            <p class="alarmWidgetP mobile">current: 11.1 C</p>
+        </div>
+        <div class="alarmWidget">
+            <h5>[12] Refrigerator</h5>
+            <p class="alarmWidgetP">Alarm: test alarm SMS en email</p>
+            <p class="alarmWidgetP mobile">Trigger: Temperature < 25 C</p>
+            <p class="alarmWidgetP mobile">Triggered on:</p>
+            <p class="alarmWidgetP mobile">2024-09-16 08:36:52</p>
+            <p class="alarmWidgetP mobile">current: 11.1 C</p>
+        </div>
+        <div class="alarmWidget">
+            <h5>[12] Refrigerator</h5>
+            <p class="alarmWidgetP">Alarm: test alarm SMS en email</p>
+            <p class="alarmWidgetP mobile">Trigger: Temperature < 25 C</p>
+            <p class="alarmWidgetP mobile">Triggered on:</p>
+            <p class="alarmWidgetP mobile">2024-09-16 08:36:52</p>
+            <p class="alarmWidgetP mobile">current: 11.1 C</p>
+        </div>
+    </div>
 </div>
 <?php 
     include('pop-up-nav.php');
--- dashboard.php
+++ dashboard.php
@@ -10,24 +10,31 @@
     <?php include("header-content.php"); ?>
     <div id="pageContent">
         <div class="contentChanger">
-            <select name="teelt" id="teeltSelector">
-                <option value="teelt1">Teelt 1</option>
-                <option value="teelt2">Teelt 2</option>
-                <option value="teelt3">Teelt 3</option>
-            </select> 
-            <label for="refreshData" class="refreshCheck">
-                <input type="checkbox" id="refreshData">
-                <p>Refresh data</p>
-            </label>
+            <div style="display: flex;">
+                <select name="teelt" id="teeltSelector">
+                    <option value="teelt1">Teelt 1</option>
+                    <option value="teelt2">Teelt 2</option>
+                    <option value="teelt3">Teelt 3</option>
+                </select> 
+                <label for="refreshData" class="refreshCheck">
+                    <input type="checkbox" id="refreshData">
+                    <p>Refresh data</p>
+                </label>
+            </div>
+            <button id="addWidgetButton">+ Add Widget</button>
         </div>
         <div class="widgetContent">
-            <div class="widget marginRight">
+            <div class="widget">
                 <h4><i class="fa-solid fa-temperature-three-quarters"></i>24h Temperature</h4>
-                <img src="img/temperature.PNG" alt="Temperature"> 
             </div> 
-            <div class="widget marginLeft">
+            <div class="widget">
                 <h4><i class="fa-solid fa-droplet"></i>Water Management</h4>
-                <img src="img/water.PNG" alt="Water management">  
+            </div>
+            <div class="widget">
+                <h4><i class="fa-solid fa-droplet"></i>Water Management</h4>
+            </div>
+            <div class="widget">
+                <h4><i class="fa-solid fa-droplet"></i>Water Management</h4>
             </div>
         </div>
     </div>
@@ -36,3 +43,12 @@
     include('pop-up-nav.php');
     readfile('footer.html');
 ?>
+
+<!-- <div class="widget marginRight">
+    <h4><i class="fa-solid fa-temperature-three-quarters"></i>24h Temperature</h4>
+    <img src="img/temperature.PNG" alt="Temperature"> 
+</div> 
+<div class="widget marginLeft">
+    <h4><i class="fa-solid fa-droplet"></i>Water Management</h4>
+    <img src="img/water.PNG" alt="Water management">  
+</div> -->
